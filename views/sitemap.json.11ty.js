441e0ce5b9315838efe458803ec92585e0f45df7
Mihai Nueleanu <mihai.nueleanu@gmail.com>
2021-03-18 21:41:10 +0100

    feat: replace relative images to absolute links

--- views/sitemap.json.11ty.js
+++ views/sitemap.json.11ty.js
@@ -1,13 +1,18 @@
 module.exports = function (data) {
   const result = data.collections.post.map((post) => {
-    const content = post.template.inputContent.replace(/---[\s\S]*?---/, "");
+    const slug = post.template.fileSlugStr;
+    const title = post.template.frontMatter.data.title;
+    const date = post.date;
 
-    return {
-      title: post.template.frontMatter.data.title,
-      date: post.date,
-      slug: post.template.fileSlugStr,
-      content,
-    };
+    // clean content
+    let content = post.template.inputContent.replace(/---[\s\S]*?---/, "");
+    content = content.replace(/(!\[.*?\]\(\.)(.+?)(\))/g, (whole, a, b, c) => {
+      a = a.replace(".", "");
+      b = "https://nueleanu.com/posts/" + slug + b;
+      return a + b + c;
+    });
+
+    return { title, date, slug, content };
   });
 
   return JSON.stringify(result, null, 4);
