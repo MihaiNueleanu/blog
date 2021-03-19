cc3433f79408f3828ee651018ebafc21c9a06de7
Mihai Nueleanu <mihai.nueleanu@gmail.com>
2021-03-19 16:10:09 +0100

    feat: filter out uses article

--- views/sitemap.json.11ty.js
+++ views/sitemap.json.11ty.js
@@ -1,22 +1,30 @@
 module.exports = function (data) {
-  const result = data.collections.post.map((post) => {
-    const slug = post.template.fileSlugStr;
-    const title = post.template.frontMatter.data.title;
-    const date = post.date;
-    const postUrl = "https://nueleanu.com/posts/" + slug;
-    const header = `# ${title}\n\n_Originally posted on [dotmethod.com](${postUrl})_\n\n`;
+  const result = data.collections.post
+    .filter((post) => post.template.fileSlugStr != "uses")
+    .map((post) => {
+      const slug = post.template.fileSlugStr;
+      const title = post.template.frontMatter.data.title;
+      const date = post.date;
+      const postUrl = "https://nueleanu.com/posts/" + slug;
+      const header = `# ${title}\n\n_Originally posted on [dotmethod.com](${postUrl})_\n\n`;
 
-    // clean content
-    let content = post.template.inputContent.replace(/---[\s\S]*?---/, header);
-    content = content.replace(/(!\[.*?\]\(\.)(.+?)(\))/g, (whole, a, b, c) => {
-      // replace images with absolute urls
-      a = a.replace(".", "");
-      b = postUrl + b;
-      return a + b + c;
+      // clean content
+      let content = post.template.inputContent.replace(
+        /---[\s\S]*?---/,
+        header
+      );
+      content = content.replace(
+        /(!\[.*?\]\(\.)(.+?)(\))/g,
+        (whole, a, b, c) => {
+          // replace images with absolute urls
+          a = a.replace(".", "");
+          b = postUrl + b;
+          return a + b + c;
+        }
+      );
+
+      return { title, date, slug, content };
     });
-
-    return { title, date, slug, content };
-  });
 
   return JSON.stringify(result, null, 4);
 };
